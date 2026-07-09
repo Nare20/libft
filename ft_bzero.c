a1911df87f951779e07d61d929cf01407873d5bf
Nare20 <Nare20>
2026-07-09 14:06:07 +0400

    Adding ft_bzer

---
+++ ft_bzero.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void explicit_bzero(void *b, size_t len){
+	unsigned char * ptr;
+	size_t i;
+	ptr = (unsigned char *) b;
+	i = 0;
+	while( i < len ){
+		ptr[i] = 0;
+		i++;
+	}
+}
